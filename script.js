8381a520f81ff6d59e2808fea9a97b631745c822
MaryMaistrova <maistrovamariia@gmail.com>
2024-10-20 21:40:35 +0300

    try 1

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const btn = document.querySelector('button');
+const btn = document.getElementById('btn');
 const red = document.querySelector('li:first-of-type');
 const yellow = document.querySelector('li:nth-of-type(2)');
 const green = document.querySelector('li:nth-of-type(3)');
 
 
-btn.onclick = function() {
-  red.style.filter = 'none';
-
-}
+btn.addEventListener('click', () => {
+  red.classList.toggle('red');
+  yellow.classList.toggle('yellow');
+  green.classList.toggle('green');
+})
